48ab6f366b4857b42f152d208bdb5613f36af6a0
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-03-30 16:55:14 +0200

    bugfixes

--- app/(components)/AccountModal.tsx
+++ app/(components)/AccountModal.tsx
@@ -34,13 +34,13 @@ const AccountModal = ({ close }: Props) => {
           <svg
             className='w-8 h-8 cursor-pointer'
             stroke='currentColor'
-            stroke-width='2.5'
+            strokeWidth='2.5'
             viewBox='0 0 24 24'
             xmlns='http://www.w3.org/2000/svg'
             aria-hidden='true'>
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
+              strokeLinecap='round'
+              strokeLinejoin='round'
               d='M6 18L18 6M6 6l12 12'></path>
           </svg>
         </div>
