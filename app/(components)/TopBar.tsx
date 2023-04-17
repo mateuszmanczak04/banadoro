abc8ce9744beff41011aaed3305476955f0250d5
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-04-17 16:54:34 +0200

    responsive top bar

--- app/(components)/TopBar.tsx
+++ app/(components)/TopBar.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useWindowWidth } from '../../hooks/useWindowWidth';
+import { TrophyIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 
 type Props = {
   openSettings: () => void;
@@ -7,13 +11,23 @@ type Props = {
 };
 
 const TopBar = ({ openSettings, openAccount, openRanking }: Props) => {
+  const width = useWindowWidth();
+
   return (
     <div className='fixed top-0 flex justify-between w-full px-4 py-2 bg-primary-300 dark:bg-gray-800 dark:text-gray-200 z-10'>
-      <h1 className='text-2xl font-extrabold'>Banadoro 🍌</h1>
+      <h1 className='sm:text-2xl text-xl font-extrabold whitespace-nowrap'>
+        Banadoro 🍌
+      </h1>
       <div className='flex gap-6'>
-        <button onClick={openRanking}>Ranking</button>
-        <button onClick={openAccount}>Account</button>
-        <button onClick={openSettings}>Settings</button>
+        <button onClick={openRanking}>
+          {width >= 640 ? 'Ranking' : <TrophyIcon className='h-6 w-6' />}
+        </button>
+        <button onClick={openAccount}>
+          {width >= 640 ? 'Account' : <UserCircleIcon className='h-6 w-6' />}
+        </button>
+        <button onClick={openSettings}>
+          {width >= 640 ? 'Setting' : <Cog6ToothIcon className='h-6 w-6' />}
+        </button>
       </div>
     </div>
   );
