c6fad3b528d5de64be85f296b5f3b5607fa0c872
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-06-05 21:20:00 +0200

    fix app height screen

--- components/App.tsx
+++ components/App.tsx
@@ -54,7 +54,7 @@ const App = () => {
         openSettings={() => setSettingsOpen(true)}
         openAccount={() => setAccountOpen(true)}
       />
-      <div className='flex flex-col items-center md:items-start gap-8 mb-8 pt-20 md:flex-row w-screen max-w-10/12 p-8 justify-center h-screen'>
+      <div className='flex flex-col items-center md:items-start gap-8 mb-8 pt-20 md:flex-row w-screen max-w-10/12 p-8 justify-center min-h-screen'>
         <div className='w-full max-w-sm flex flex-col gap-8'>
           <ClockFrame />
           <AddTaskForm />
