75f69a179b5ffc397fe497d35cfaf3f37d358578
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-06-10 15:03:44 +0200

    fix width of subpages

--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -2,7 +2,7 @@ import Ranking from '@/components/ranking/Ranking';
 
 const page = () => {
   return (
-    <div className='w-full max-w-xl mt-20 mx-auto flex flex-col gap-4'>
+    <div className='w-11/12 max-w-xl mt-20 mx-auto flex flex-col gap-4'>
       <h1 className='text-3xl font-bold'>Ranking</h1>
       <Ranking />
     </div>
