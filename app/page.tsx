28a0df7dfead612e90bb880420fef1a22d95bde3
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-06-12 11:40:37 +0200

    add footer in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import App from '@/components/home/App';
 import Description from '@/components/home/Description';
+import Footer from '@/components/home/Footer';
 
 const page = () => {
   return (
     <>
       <App />
       <Description />
+      <Footer />
     </>
   );
 };
---
+++ components/home/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='w-full bg-gray-900 text-gray-400 flex justify-center py-8'>
+      <div className='w-11/12 max-w-md text-center flex flex-col items-center gap-4'>
+        <a href='https://www.linkedin.com/in/mateusz-manczak/' target='_blank'>
+          Made by Mateusz Mańczak
+        </a>
+        <a
+          href='https://github.com/mateuszmanczak04/banadoro'
+          className='bg-primary-500 text-gray-900 px-2 rounded'>
+          Source code
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
