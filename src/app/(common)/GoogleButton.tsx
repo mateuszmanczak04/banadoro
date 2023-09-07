7ff6db3f103aca4850f2006f52adb60b13733ab4
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-09-07 14:24:33 +0200

    change google button design

--- src/app/(common)/GoogleButton.tsx
+++ src/app/(common)/GoogleButton.tsx
@@ -11,7 +11,9 @@ const GoogleButton = ({ text }: { text: string }) => {
   };
 
   return (
-    <button className='google-button' onClick={handleLogin}>
+    <button
+      className='w-full py-1 px-3 rounded shadow cursor-pointer transition text-center text-white flex items-center justify-center gap-2 bg-gray-700 duration-300 hover:bg-gray-600'
+      onClick={handleLogin}>
       <Image src='/google-icon.svg' width={20} height={20} alt='google icon' />
       <p>{text}</p>
     </button>
