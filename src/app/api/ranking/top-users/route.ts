9a9df62f67750331d7e18d9f9d3de94a62ee798b
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-09-13 15:50:19 +0200

    replace ranking with client component

---
+++ src/app/api/ranking/top-users/route.ts
@@ -0,0 +1,20 @@
+import dbConnect from '@/lib/dbConnect';
+import User from '@/models/User';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const topUsers: RankingTopUser[] = await User.find()
+      .select({ totalTime: true, username: true })
+      .sort({ totalTime: -1 });
+
+    return NextResponse.json(topUsers);
+  } catch {
+    return NextResponse.json(
+      { message: 'Something went wrong.' },
+      { status: 500 }
+    );
+  }
+}
--- src/app/ranking/Ranking.tsx
+++ src/app/ranking/Ranking.tsx
@@ -1,37 +1,52 @@
-import dbConnect from '@/lib/dbConnect';
-import User from '@/models/User';
+'use client';
 
-// export const revalidate = 10;
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Loading from '../loading';
 
-export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-no-store';
+const Ranking = () => {
+  const [topUsers, setTopUsers] = useState<RankingTopUser[]>([]);
+  const [error, setError] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-const Ranking = async () => {
-  await dbConnect();
-
-  const topUsers: RankingTopUser[] = await User.find()
-    .select({ totalTime: true, username: true })
-    .sort({ totalTime: -1 });
+  useEffect(() => {
+    const fetchTopUsers = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get('/api/ranking/top-users');
+        setTopUsers(res.data);
+      } catch {
+        setError('Something went wrong');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTopUsers();
+  }, []);
 
   return (
     <div className='flex flex-col items-center gap-4 w-full'>
       <div className='flex flex-col gap-2 w-full'>
-        {topUsers.map((user: User, index: number) => (
-          <div
-            key={user._id}
-            className='flex flex-col gap-2 md:flex-row justify-between items-center border-2 border-opacity-50 rounded p-2 bg-gray-800 border-gray-700'>
-            <p
-              className={`w-6 h-6 flex justify-center items-center rounded ${
-                index === 0 && 'bg-primary-400 text-primary-800'
-              } ${index === 1 && 'bg-gray-300 text-gray-800'} ${
-                index === 2 && 'bg-primary-700 text-primary-400'
-              }`}>
-              {index + 1}
-            </p>
-            <p>{user.username}</p>
-            <p>{user.totalTime} minutes</p>
-          </div>
-        ))}
+        {isLoading && <Loading />}
+        {error && <p>{error}</p>}
+        {topUsers &&
+          topUsers.length > 0 &&
+          topUsers.map((user: User, index: number) => (
+            <div
+              key={user._id}
+              className='flex flex-col gap-2 md:flex-row justify-between items-center border-2 border-opacity-50 rounded p-2 bg-gray-800 border-gray-700'>
+              <p
+                className={`w-6 h-6 flex justify-center items-center rounded ${
+                  index === 0 && 'bg-primary-400 text-primary-800'
+                } ${index === 1 && 'bg-gray-300 text-gray-800'} ${
+                  index === 2 && 'bg-primary-700 text-primary-400'
+                }`}>
+                {index + 1}
+              </p>
+              <p>{user.username}</p>
+              <p>{user.totalTime} minutes</p>
+            </div>
+          ))}
       </div>
     </div>
   );
