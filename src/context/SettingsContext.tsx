225f82f8f6e1685c47f8c558b1e6303f5211700a
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-09-15 09:15:05 +0200

    fix local storage step 1

--- src/context/SettingsContext.tsx
+++ src/context/SettingsContext.tsx
@@ -1,3 +1,4 @@
+import { init } from 'next/dist/compiled/@vercel/og/satori';
 import { createContext, FC, ReactNode, useEffect, useState } from 'react';
 
 interface SettingsContextProps {
@@ -11,62 +12,74 @@ interface SettingsContextProps {
   setBreakTime: (time: number) => void;
 }
 
-const initialState: SettingsContextProps = {
-  autoStart: localStorage.getItem('autoStart') === 'true',
-  setAutoStart: () => {},
-  hasAccount: localStorage.getItem('hasAccount') === 'true' ? true : false,
-  setHasAccount: () => {},
-  sessionTime: localStorage.getItem('sessionTime')
-    ? parseInt(localStorage.getItem('sessionTime') as string)
-    : 25 * 60,
-  breakTime: localStorage.getItem('breakTime')
-    ? parseInt(localStorage.getItem('breakTime') as string)
-    : 5 * 60,
-  setSessionTime: () => {},
-  setBreakTime: () => {},
-};
-
-export const SettingsContext =
-  createContext<SettingsContextProps>(initialState);
+export const SettingsContext = createContext<SettingsContextProps>(
+  {} as SettingsContextProps
+);
 
 export const SettingsContextProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [autoStart, setAutoStart] = useState<boolean>(initialState.autoStart);
-  const [hasAccount, setHasAccount] = useState<boolean>(
-    initialState.hasAccount
-  );
-  const [sessionTime, setSessionTime] = useState(initialState.sessionTime);
-  const [breakTime, setBreakTime] = useState(initialState.breakTime);
+  const [autoStart, setAutoStart] = useState<boolean>(false);
+  const [hasAccount, setHasAccount] = useState<boolean>(false);
+  const [sessionTime, setSessionTime] = useState<number>(25 * 60);
+  const [breakTime, setBreakTime] = useState<number>(5 * 60);
 
   useEffect(() => {
-    if (hasAccount)
-      localStorage.setItem('hasAccount', JSON.stringify(hasAccount));
-  }, [hasAccount]);
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('autoStart'))
+        setAutoStart(localStorage.getItem('autoStart') === 'true');
+      if (localStorage.getItem('hasAccount'))
+        setHasAccount(localStorage.getItem('hasAccount') === 'true');
+      if (localStorage.getItem('sessionTime'))
+        setSessionTime(
+          localStorage.getItem('sessionTime')
+            ? parseInt(localStorage.getItem('sessionTime') as string)
+            : 25 * 60
+        );
+      if (localStorage.getItem('breakTime'))
+        setBreakTime(
+          localStorage.getItem('breakTime')
+            ? parseInt(localStorage.getItem('breakTime') as string)
+            : 5 * 60
+        );
+    }
+  }, []);
 
-  useEffect(() => {
+  const lsSetAutoStart = (
+    autoStart: boolean | ((prev: boolean) => boolean)
+  ) => {
+    setAutoStart(autoStart);
     localStorage.setItem('autoStart', JSON.stringify(autoStart));
-  }, [autoStart]);
+  };
 
-  useEffect(() => {
+  const lsSetHasAccount = (
+    hasAccount: boolean | ((prev: boolean) => boolean)
+  ) => {
+    setHasAccount(hasAccount);
+    localStorage.setItem('hasAccount', JSON.stringify(hasAccount));
+  };
+
+  const lsSetSessionTime = (sessionTime: number) => {
+    setSessionTime(sessionTime);
     localStorage.setItem('sessionTime', JSON.stringify(sessionTime));
-  }, [sessionTime]);
+  };
 
-  useEffect(() => {
+  const lsSetBreakTime = (breakTime: number) => {
+    setBreakTime(breakTime);
     localStorage.setItem('breakTime', JSON.stringify(breakTime));
-  }, [breakTime]);
+  };
 
   return (
     <SettingsContext.Provider
       value={{
         autoStart,
-        setAutoStart,
+        setAutoStart: lsSetAutoStart,
         hasAccount,
-        setHasAccount,
+        setHasAccount: lsSetHasAccount,
         sessionTime,
+        setSessionTime: lsSetSessionTime,
         breakTime,
-        setSessionTime,
-        setBreakTime,
+        setBreakTime: lsSetBreakTime,
       }}>
       {children}
     </SettingsContext.Provider>
