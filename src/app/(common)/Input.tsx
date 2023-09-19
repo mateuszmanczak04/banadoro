1b063d5e84bd5cc2ecc6110019663608758d35d8
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-09-19 14:06:53 +0200

    fix input type password not showing properly

--- src/app/(common)/Input.tsx
+++ src/app/(common)/Input.tsx
@@ -2,61 +2,45 @@
 
 import twClass from '@/lib/twClass';
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
-import { FC, InputHTMLAttributes, useState } from 'react';
-
-// interface Props {
-//   value: string;
-//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-//   type?: string;
-//   placeholder?: string;
-// }
+import { FC, InputHTMLAttributes, useRef, useState } from 'react';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {}
 
 const Input: FC<Props> = ({ className, ...props }) => {
   const [show, setShow] = useState<boolean>(false);
+  const ref = useRef<any>();
 
-  const handleToggle = () => {
-    setShow((prev) => !prev);
-  };
-
-  if (props.type === 'password') {
+  if (props.type === 'password')
     return (
-      <>
+      <div className='relative w-full'>
+        <input
+          ref={ref}
+          type={show ? 'text' : 'password'}
+          className={twClass(
+            'rounded w-full p-2 pr-10 bg-gray-700 text-gray-200 border-2 border-gray-600 transition duration-100 placeholder:text-gray-500 outline-none focus:border-primary-300 focus:border-opacity-30',
+            className
+          )}
+          {...props}
+        />
         {show ? (
-          <div className='relative w-full'>
-            <input
-              type='text'
-              className={twClass(
-                'rounded w-full p-2 pr-10 bg-gray-700 text-gray-200 border-2 border-gray-600 transition duration-100 placeholder:text-gray-500 outline-none focus:border-primary-300 focus:border-opacity-30',
-                className
-              )}
-              {...props}
-            />
-            <EyeSlashIcon
-              className='h-6 w-6 text-gray-500 absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 select-none'
-              onClick={handleToggle}
-            />
-          </div>
+          <EyeSlashIcon
+            className='h-6 w-6 text-gray-500 absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 select-none'
+            onClick={() => {
+              setShow(false);
+              ref.current.type = 'password';
+            }}
+          />
         ) : (
-          <div className='relative w-full'>
-            <input
-              type='password'
-              className={twClass(
-                'rounded w-full p-2 pr-10 bg-gray-700 text-gray-200 border-2 border-gray-600 transition duration-100 placeholder:text-gray-500 outline-none focus:border-primary-300 focus:border-opacity-30',
-                className
-              )}
-              {...props}
-            />
-            <EyeIcon
-              className='h-6 w-6 text-gray-300 absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 select-none'
-              onClick={handleToggle}
-            />
-          </div>
+          <EyeIcon
+            className='h-6 w-6 text-gray-300 absolute right-3 cursor-pointer top-1/2 -translate-y-1/2 select-none'
+            onClick={() => {
+              setShow(true);
+              ref.current.type = 'text';
+            }}
+          />
         )}
-      </>
+      </div>
     );
-  }
 
   return (
     <input
