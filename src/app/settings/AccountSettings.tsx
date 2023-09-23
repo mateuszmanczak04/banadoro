40f2a2b6f0a15ee6307dc512367bf70cff8582fb
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-09-23 09:23:52 +0200

    fix display of account settings

--- src/app/settings/AccountSettings.tsx
+++ src/app/settings/AccountSettings.tsx
@@ -1,7 +1,14 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
 import SignOutButton from '../(common)/SignOutButton';
 import ChangePassword from './ChangePassword';
 
 const AccountSettings = () => {
+  const { data: session } = useSession();
+
+  if (!session) return null;
+
   return (
     <div className='flex flex-col gap-4 w-full'>
       <h3 className='font-extrabold text-3xl w-full'>Account</h3>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,14 +1,11 @@
-import { getAuthSession } from '@/lib/auth';
 import AccountSettings from './AccountSettings';
 import TimerSettings from './TimerSettings';
 
 const page = async () => {
-  const session = await getAuthSession();
-
   return (
     <div className='mt-28 w-11/12 max-w-xl mx-auto flex flex-col items-center gap-12'>
       <TimerSettings />
-      {session && <AccountSettings />}
+      <AccountSettings />
     </div>
   );
 };
