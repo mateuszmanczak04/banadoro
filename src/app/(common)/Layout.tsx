71f55cba3507d20874ce51d72c94b383dbb6556b
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-02-12 12:47:07 +0100

    feat: add indicator telling that user is offline on main screen

--- src/app/(common)/Layout.tsx
+++ src/app/(common)/Layout.tsx
@@ -5,29 +5,36 @@ import TimerModal from './TimerModal';
 import Navigation from './Navigation';
 import { useSession } from 'next-auth/react';
 import Loading from './Loading';
+import useOnlineStatusContext from '@/hooks/useOnlineStatusContext';
 
 interface Props {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
-  const { status } = useSession();
+	const { status } = useSession();
+	const { online } = useOnlineStatusContext();
 
-  if (status === 'loading')
-    return (
-      <div className='bottom-menu container'>
-        <Loading />
-      </div>
-    );
+	if (status === 'loading')
+		return (
+			<div className='bottom-menu container'>
+				<Loading />
+			</div>
+		);
 
-  return (
-    <>
-      {/* @ts-ignore server component */}
-      <Navigation />
-      <TimerModal />
-      <div>{children}</div>
-    </>
-  );
+	return (
+		<>
+			{/* @ts-ignore server component */}
+			{!online && (
+				<div className='fixed bottom-16 xs:bottom-0 h-[32px] leading-[32px] inset-x-0 text-white bg-red-500 text-center px-2'>
+					You are offline
+				</div>
+			)}
+			<Navigation />
+			<TimerModal />
+			<div>{children}</div>
+		</>
+	);
 };
 
 export default Layout;
