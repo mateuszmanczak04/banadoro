4a0169c0cc790bba8c9da59b16d981bf0983f35c
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-02-21 18:20:50 +0100

    fix caching with sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,54 @@
 const CACHE_NAME = 'v1';
 
-self.addEventListener('install', () => {
-	console.log('service worker installed');
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache => {
+			return cache.addAll([
+				'/', // Cache the index page
+				'/settings',
+				'/stats',
+				'/signin',
+				'/signup',
+				'/ranking',
+				// Add other routes here
+			]);
+		}),
+	);
 });
 
-self.addEventListener('activate', () => {
-	console.log('service worker activated');
+self.addEventListener('activate', event => {
+	console.log('Service worker activated');
 });
 
-const cacheClone = async e => {
-	const res = await fetch(e.request);
-	const resClone = res.clone();
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.open(CACHE_NAME).then(async cache => {
+			const cachedResponse = await cache.match(event.request);
+			if (cachedResponse) {
+				return cachedResponse;
+			}
 
-	const cache = await caches.open(CACHE_NAME);
-	await cache.put(e.request, resClone);
-	return res;
-};
+			// If request is not cached, fetch it from network
+			return fetch(event.request)
+				.then(networkResponse => {
+					// Clone the response as it can be consumed only once
+					const responseToCache = networkResponse.clone();
 
-self.addEventListener('fetch', e => {
-	e.respondWith(
-		cacheClone(e)
-			.catch(() => caches.match(e.request))
-			.then(res => res),
+					// Cache the fetched resource
+					caches.open(CACHE_NAME).then(cache => {
+						cache.put(event.request, responseToCache);
+					});
+
+					// Return the network response
+					return networkResponse;
+				})
+				.catch(() => {
+					// If fetch fails and there's no cached response, return an error response
+					return new Response('Offline', {
+						status: 503,
+						statusText: 'Service Unavailable',
+					});
+				});
+		}),
 	);
 });
